5e2bb7ca3817f79982e4cc4bc9cc37267d3332be
ccycle <ccycle713@gmail.com>
2022-08-15 10:25:26 +0900

    Remove `SGRLogInfo`

--- src/Herp/Logger/ANSI/Coloring.hs
+++ src/Herp/Logger/ANSI/Coloring.hs
@@ -6,30 +6,25 @@ import Herp.Logger.LogLevel (LogLevel (..))
 import System.Console.ANSI
 
 -- https://hackage.haskell.org/package/ansi-terminal-0.11.3/docs/System-Console-ANSI-Types.html#t:SGR
-data SGRLogInfo = NotColored | SGRLogInfo [SGR]
 
-logLevelToColoredLogInfo :: LogLevel -> SGRLogInfo
-logLevelToColoredLogInfo level = case level of
-    Debug -> SGRLogInfo [SetRGBColor Background $ sRGB24 196 196 196] -- Grey
-    Informational -> SGRLogInfo [SetRGBColor Background $ sRGB24 155 204 228] -- Light blue
-    Notice -> SGRLogInfo [SetRGBColor Background $ sRGB24 154 169 186] -- Grey
-    Warning -> SGRLogInfo [SetRGBColor Background $ sRGB24 237 179 89] -- Orange
-    Error -> SGRLogInfo [SetRGBColor Background $ sRGB24 226 86 77] -- Red
-    Critical -> SGRLogInfo [SetRGBColor Background $ sRGB24 202 9 10] -- Wine red
-    Alert -> SGRLogInfo [SetRGBColor Background $ sRGB24 152 0 0] -- #980000
-    Emergency -> SGRLogInfo [SetRGBColor Background $ sRGB24 101 0 0] --- #650000
+logLevelToSGR :: LogLevel -> [SGR]
+logLevelToSGR level = case level of
+    Debug -> [SetRGBColor Background $ sRGB24 196 196 196] -- Grey
+    Informational -> [SetRGBColor Background $ sRGB24 155 204 228] -- Light blue
+    Notice -> [SetRGBColor Background $ sRGB24 154 169 186] -- Grey
+    Warning -> [SetRGBColor Background $ sRGB24 237 179 89] -- Orange
+    Error -> [SetRGBColor Background $ sRGB24 226 86 77] -- Red
+    Critical -> [SetRGBColor Background $ sRGB24 202 9 10] -- Wine red
+    Alert -> [SetRGBColor Background $ sRGB24 152 0 0] -- #980000
+    Emergency -> [SetRGBColor Background $ sRGB24 101 0 0] --- #650000
 
 setLogColorStr :: IsString a => LogLevel -> a
 setLogColorStr level =
-    let sgrLogInfo = logLevelToColoredLogInfo level
-     in case sgrLogInfo of
-            NotColored -> ""
-            SGRLogInfo sgr ->
-                fromString (setSGRCode sgr)
+    let sgrLogInfo = logLevelToSGR level
+     in fromString (setSGRCode sgrLogInfo)
 
 resetLogColorStr :: IsString a => a
 resetLogColorStr = fromString (setSGRCode [Reset])
 
 coloringLogInfoStr :: (IsString a, Semigroup a) => LogLevel -> a -> a
--- coloringLogInfoStr level str = setLogColorStr level <> str <> resetLogColorStr
 coloringLogInfoStr level str = setLogColorStr level <> " " <> resetLogColorStr <> " " <> str
